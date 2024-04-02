7356523effb0776eac1f8dcf1436e01a7f617c1c
mikesplore <mikepremium8@gmail.com>
2024-04-02 07:48:13 +0300

    e

--- mySchool/delete.cs
+++ mySchool/delete.cs
@@ -51,7 +51,7 @@ namespace mySchool
                     string gender = reader["gender"].ToString();
                     string studentClass = reader["myclass"].ToString();
 
-                    // Display or use the retrieved information as ne
+                    // Display or use the retrieved information as nee
                     txtLName.Text =lastName;
                     txtFName.Text =firstName;
                     txtDOB.Text = dob;
